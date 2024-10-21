c9ccf3ddb9b062d7db040fd20115c4b500c97c98
Kyle <kyleagren@gmail.com>
2024-10-21 14:36:44 -0600

    Fix all tests aside from "Bad Team Color"

--- server/src/main/java/dataaccess/GameDataAccessMemory.java
+++ server/src/main/java/dataaccess/GameDataAccessMemory.java
@@ -6,13 +6,13 @@ import model.GameData;
 import java.util.ArrayList;
 
 public class GameDataAccessMemory implements GameDataAccess {
-    private static ArrayList<GameData> games = new ArrayList<>();
+    private static final ArrayList<GameData> games = new ArrayList<>();
 
     @Override
     public int createGame(String gameName) {
         int currentSize = games.size();
         ChessGame game = new ChessGame();
-        GameData gameRepresentation = new GameData(currentSize + 1, "", "", gameName, game);
+        GameData gameRepresentation = new GameData(currentSize + 1, null, null, gameName, game);
         games.add(gameRepresentation);
         return (currentSize + 1);
     }
@@ -28,47 +28,47 @@ public class GameDataAccessMemory implements GameDataAccess {
 
     @Override
     public void joinGame(int gameID, String playerColor, String username) throws DataAccessException {
-        GameData game = null;
+        GameData game;
         try {
-            game = getGame(gameID - 1);
+            game = getGame(gameID);
         } catch (DataAccessException e) {
             throw new DataAccessException(e.getMessage());
         }
         if (playerColor.equals("WHITE")) {
             GameData newGame;
-            if (!game.blackUsername().isEmpty()) {
+            if (game.blackUsername() != null) {
                 newGame = new GameData(gameID, username, game.blackUsername(), game.gameName(), game.game());
             }
             else {
-                newGame = new GameData(gameID, username, "", game.gameName(), game.game());
+                newGame = new GameData(gameID, username, null, game.gameName(), game.game());
             }
-            games.set(gameID, newGame);
+            games.set(gameID - 1, newGame);
         }
         else if (playerColor.equals("BLACK")) {
             GameData newGame;
-            if (!game.whiteUsername().isEmpty()) {
+            if (game.whiteUsername() != null) {
                 newGame = new GameData(gameID, game.whiteUsername(), username, game.gameName(), game.game());
             }
             else {
-                newGame = new GameData(gameID, "", username, game.gameName(), game.game());
+                newGame = new GameData(gameID, null, username, game.gameName(), game.game());
             }
-            games.set(gameID, newGame);
+            games.set(gameID - 1, newGame);
         }
     }
 
     @Override
     public boolean checkAvailability(int gameID, String playerColor) throws DataAccessException {
-        GameData game = null;
+        GameData game;
         try {
-            game = getGame(gameID - 1);
+            game = getGame(gameID);
         } catch (DataAccessException e) {
             throw new DataAccessException(e.getMessage());
         }
         if (playerColor.equals("WHITE")) {
-            return game.whiteUsername().isEmpty();
+            return game.whiteUsername() == null;
         }
         else if (playerColor.equals("BLACK")) {
-            return game.blackUsername().isEmpty();
+            return game.blackUsername() == null;
         }
         throw new DataAccessException("invalid color provided");
     }
