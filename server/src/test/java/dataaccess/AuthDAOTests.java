13c417aca40296f482b07865801757b60b00223f
Kyle <kyleagren@gmail.com>
2024-10-31 09:14:38 -0600

    Get most auth tests done.

--- server/src/test/java/dataaccess/AuthDAOTests.java
+++ server/src/test/java/dataaccess/AuthDAOTests.java
@@ -64,36 +64,36 @@ public class AuthDAOTests {
     @Test
     @Order(2)
     public void createAuthFailure() {
-
+        // TODO - how do I fail here as well?
     }
 
     @Test
     @Order(3)
     public void getAuthSuccess() {
-        Assertions.assertDoesNotThrow(() -> authDAO.getAuth())
+        Assertions.assertDoesNotThrow(() -> authDAO.getAuth(goodToken));
     }
 
     @Test
     @Order(4)
     public void getAuthFailure() {
-
+        Assertions.assertThrows(Exception.class, () -> authDAO.getAuth(UUID.randomUUID().toString()));
     }
 
     @Test
     @Order(5)
     public void deleteAuthSuccess() {
-
+        Assertions.assertDoesNotThrow(() -> authDAO.deleteAuth(goodToken));
     }
 
     @Test
     @Order(6)
     public void deleteAuthFailure() {
-
+        Assertions.assertThrows(Exception.class, () -> authDAO.deleteAuth(goodToken));
     }
 
     @Test
     @Order(7)
     public void deleteAllSuccess() {
-
+        Assertions.assertDoesNotThrow(() -> authDAO.deleteAll());
     }
 }
