fb6706e29d34a976857bb05fc87f076bfb9a6a62
Kyle <kyleagren@gmail.com>
2024-11-16 15:23:01 -0700

    Pull out code and create client communicator for better single responsibility usage

---
+++ client/src/main/java/client/ClientCommunicator.java
@@ -0,0 +1,78 @@
+package client;
+
+import com.google.gson.Gson;
+import exception.ResponseException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
+public class ClientCommunicator {
+    private String serverUrl;
+
+    public ClientCommunicator(String url) {
+        serverUrl = url;
+    }
+
+    public <T> T makeRequest(String method, String path, Object request, Class<T> responseClass, String token) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            if (!token.isEmpty()) {
+                addAuthorization(http, token);
+            }
+            writeBody(request, http);
+
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (Exception e) {
+            throw new ResponseException(500, e.getMessage());
+        }
+    }
+
+    public static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    public void addAuthorization(HttpURLConnection http, String token) {
+        http.addRequestProperty("Authorization", token);
+    }
+
+    public static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+    public void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            throw new ResponseException(status, "failure: " + status);
+        }
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+}
--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -20,101 +20,45 @@ import java.net.URL;
 
 public class ServerFacade {
     private final String serverUrl;
+    private ClientCommunicator communicator;
 
     public ServerFacade(String url) {
         serverUrl = url;
+        communicator = new ClientCommunicator(url);
     }
 
     public AuthData register(UserData data) throws ResponseException {
         var path = "/user";
-        return this.makeRequest("POST", path, data, AuthData.class, "");
+        return communicator.makeRequest("POST", path, data, AuthData.class, "");
     }
 
     public AuthData login(UserData data) throws ResponseException {
         var path = "/session";
-        return this.makeRequest("POST", path, data, AuthData.class, "");
+        return communicator.makeRequest("POST", path, data, AuthData.class, "");
     }
 
     public EmptySuccessResponse logout(String token) throws ResponseException {
         var path = "/session";
-        return this.makeRequest("DELETE", path, null, EmptySuccessResponse.class, token);
+        return communicator.makeRequest("DELETE", path, null, EmptySuccessResponse.class, token);
     }
 
     public GameCreatedResponse createGame(GameData data, String token) throws ResponseException {
         var path = "/game";
-        return this.makeRequest("POST", path, data, GameCreatedResponse.class, token);
+        return communicator.makeRequest("POST", path, data, GameCreatedResponse.class, token);
     }
 
     public EmptySuccessResponse joinGame(JoinGameRequestBody info, String token) throws ResponseException {
         var path = "/game";
-        return this.makeRequest("PUT", path, info, EmptySuccessResponse.class, token);
+        return communicator.makeRequest("PUT", path, info, EmptySuccessResponse.class, token);
     }
 
     public GamesListResponse listGames(String token) throws ResponseException {
         var path = "/game";
-        return this.makeRequest("GET", path, null, GamesListResponse.class, token);
+        return communicator.makeRequest("GET", path, null, GamesListResponse.class, token);
     }
 
     public EmptySuccessResponse clear() throws ResponseException {
         var path = "/db";
-        return this.makeRequest("DELETE", path, null, EmptySuccessResponse.class, "");
-    }
-
-    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass, String token) throws ResponseException {
-        try {
-            URL url = (new URI(serverUrl + path)).toURL();
-            HttpURLConnection http = (HttpURLConnection) url.openConnection();
-            http.setRequestMethod(method);
-            http.setDoOutput(true);
-
-            if (!token.isEmpty()) {
-                addAuthorization(http, token);
-            }
-            writeBody(request, http);
-
-            http.connect();
-            throwIfNotSuccessful(http);
-            return readBody(http, responseClass);
-        } catch (Exception e) {
-            throw new ResponseException(500, e.getMessage());
-        }
-    }
-
-    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
-        if (request != null) {
-            http.addRequestProperty("Content-Type", "application/json");
-            String reqData = new Gson().toJson(request);
-            try (OutputStream reqBody = http.getOutputStream()) {
-                reqBody.write(reqData.getBytes());
-            }
-        }
-    }
-
-    private void addAuthorization(HttpURLConnection http, String token) {
-        http.addRequestProperty("Authorization", token);
-    }
-
-    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
-        T response = null;
-        if (http.getContentLength() < 0) {
-            try (InputStream respBody = http.getInputStream()) {
-                InputStreamReader reader = new InputStreamReader(respBody);
-                if (responseClass != null) {
-                    response = new Gson().fromJson(reader, responseClass);
-                }
-            }
-        }
-        return response;
-    }
-
-    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
-        var status = http.getResponseCode();
-        if (!isSuccessful(status)) {
-            throw new ResponseException(status, "failure: " + status);
-        }
-    }
-
-    private boolean isSuccessful(int status) {
-        return status / 100 == 2;
+        return communicator.makeRequest("DELETE", path, null, EmptySuccessResponse.class, "");
     }
 }
